858efae72f76517d26bbc9e36940599f6ddab846
Oeslei Kuhn <oesleik@gmail.com>
2015-10-31 18:50:54 -0200

    Some style for the entities and listen for the keyboard to start / pause / resume the game

--- source/game-engine.js
+++ source/game-engine.js
@@ -1,26 +1,75 @@
 var loop = require('./game-loop');
+var Entity = require('./entity');
+var keyboard = require('./keyboard');
 
+var Player = require('./entities/player');
+
+var state;
+var frames;
 var entities;
+var scope;
 
 function start() {
+	if (state === 1) {
+		return;
+	}
+
+	state = 1;
+	frames = 0;
 	entities = [];
 
+	scope = document.getElementById('game');
+	scope.innerHTML = '';
+
+	addEntity(new Player());
+
+	keyboard.whenPress(keyboard.keys.SPACE, toggleState);
 	loop.start(tick);
+
+	console.log('game started');
 }
 
 function tick(elapsed) {
+	frames++;
 	update(elapsed);
 	render();
 }
 
 function update(elapsed) {
-
+	entities.forEach(function(entity) {
+		entity.update(elapsed);
+	});
 }
 
 function render() {
+	entities.forEach(function(entity) {
+		entity.render(scope);
+	});
+}
 
+function addEntity(entity) {
+	if (entity instanceof Entity) {
+		entities.push(entity);
+	} else {
+		throw 'The object given must be an entity';
+	}
+}
+
+function toggleState() {
+	if (state === 1) {
+		state = 2;
+		loop.pause();
+
+		console.log('game paused');
+	} else if (state === 2) {
+		state = 1;
+		loop.resume();
+
+		console.log('game resumed');
+	}
 }
 
 module.exports = {
-	start: start
+	start: start,
+	addEntity: addEntity
 };
--- source/main.js
+++ source/main.js
@@ -1,3 +1,4 @@
 var game = require('./game-engine');
+var keyboard = require('./keyboard');
 
-// game.start();
+keyboard.whenPress(keyboard.keys.SPACE, game.start, true);
