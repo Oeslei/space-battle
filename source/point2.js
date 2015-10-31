a42715ba908a97df6477a98275a048bb261f878a
Oeslei Kuhn <oesleik@gmail.com>
2015-10-31 18:48:03 -0200

    Modules vector2 and point2

---
+++ source/point2.js
@@ -0,0 +1,8 @@
+function point2(x, y) {
+	return {
+		x: x || 0,
+		y: y || 0
+	};
+}
+
+module.exports = point2;
---
+++ source/vector2.js
@@ -0,0 +1,8 @@
+function vector2(x, y) {
+	return {
+		x: x || 0,
+		y: y || 0
+	};
+}
+
+module.exports = vector2;
